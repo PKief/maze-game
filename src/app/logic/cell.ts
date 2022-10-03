f9bbc0bd5e3b7f2883cee1c88af6157b05babe8a
Philipp Kief <philipp.kief@gmx.de>
2022-10-03 17:06:38 +0200

    Implement hunt and kill algorithm

--- src/app/logic/cell.ts
+++ src/app/logic/cell.ts
@@ -5,7 +5,10 @@ export class Cell {
   visited: boolean;
   walls: Walls;
 
-  constructor(private _position: Position) {
+  constructor(
+    private _position: Position,
+    private readonly mazeSize: Position
+  ) {
     this.visited = false;
     this.walls = {
       north: true,
@@ -19,13 +22,13 @@ export class Cell {
     return this._position;
   }
 
-  getNeighbors(mazeSize: Position): Position[] {
+  getNeighbors(): Position[] {
     const x = this._position[0];
     const y = this._position[1];
 
     const north = y > 0 ? [x, y - 1] : undefined;
-    const east = x < mazeSize[0] - 1 ? [x + 1, y] : undefined;
-    const south = y < mazeSize[1] - 1 ? [x, y + 1] : undefined;
+    const east = x < this.mazeSize[0] - 1 ? [x + 1, y] : undefined;
+    const south = y < this.mazeSize[1] - 1 ? [x, y + 1] : undefined;
     const west = x > 0 ? [x - 1, y] : undefined;
 
     return [north, east, south, west].filter(
--- src/app/logic/maze.ts
+++ src/app/logic/maze.ts
@@ -8,32 +8,88 @@ export class Maze {
   constructor(private mazeSize: Position) {
     for (let r = 0; r < mazeSize[0]; r++) {
       for (let c = 0; c < mazeSize[1]; c++) {
-        const cell = new Cell([r, c]);
+        const cell = new Cell([r, c], mazeSize);
         this.cells.push(cell);
       }
     }
 
-    this.recursiveBacktrackingGeneration(this.cells[0]);
+    this.huntAndKill();
   }
 
-  private recursiveBacktrackingGeneration(currentCell: Cell) {
-    currentCell.visited = true;
-    const neighbors = currentCell
-      .getNeighbors(this.mazeSize)
-      .map(
-        (neighbor) =>
-          this.cells.find(
-            (c) =>
-              c.position[0] === neighbor[0] && c.position[1] === neighbor[1]
-          ) as Cell
-      );
+  private recursiveBacktracking() {
+    const visitNextCell = (currentCell: Cell) => {
+      currentCell.visited = true;
+      const neighbors = currentCell
+        .getNeighbors()
+        .map(this.mapNeighborPositionsToCells());
 
-    shuffleArray(neighbors).forEach((neighbor) => {
-      if (neighbor.visited === false) {
-        this.updateWall(currentCell, neighbor);
-        this.recursiveBacktrackingGeneration(neighbor);
+      shuffleArray(neighbors).forEach((neighbor) => {
+        if (neighbor.visited === false) {
+          this.updateWall(currentCell, neighbor);
+          visitNextCell(neighbor);
+        }
+      });
+    };
+
+    visitNextCell(this.cells[0]);
+  }
+
+  private huntAndKill() {
+    const visitNextCell = (currentCell: Cell) => {
+      currentCell.visited = true;
+      const neighbors = currentCell
+        .getNeighbors()
+        .map(this.mapNeighborPositionsToCells());
+
+      const nextNeighbor = shuffleArray(neighbors).filter(
+        (n) => n.visited === false
+      )[0];
+
+      if (nextNeighbor) {
+        this.updateWall(currentCell, nextNeighbor);
+        visitNextCell(nextNeighbor);
+      } else {
+        return;
       }
-    });
+    };
+
+    const startCell = shuffleArray(this.cells)[0];
+    visitNextCell(startCell);
+
+    const getNextUnvisitedCell = () => {
+      return this.cells
+        .filter((cell) => cell.visited === false)
+        .find((unvisitedCell) => {
+          return (
+            unvisitedCell
+              .getNeighbors()
+              .map(this.mapNeighborPositionsToCells())
+              .filter((neighbor) => neighbor.visited === true).length > 0
+          );
+        });
+    };
+
+    let nextCell = getNextUnvisitedCell();
+    while (nextCell) {
+      const nextVisitedNeighbor = shuffleArray(
+        nextCell
+          .getNeighbors()
+          .map(this.mapNeighborPositionsToCells())
+          .filter((neighbor) => neighbor.visited === true)
+      )[0];
+
+      this.updateWall(nextCell, nextVisitedNeighbor);
+      visitNextCell(nextCell);
+      nextCell = getNextUnvisitedCell();
+    }
+  }
+
+  private mapNeighborPositionsToCells() {
+    return (neighbor: Position) => {
+      return this.cells.find(
+        (c) => c.position[0] === neighbor[0] && c.position[1] === neighbor[1]
+      ) as Cell;
+    };
   }
 
   private updateWall(currentCell: Cell, neighbor: Cell) {
