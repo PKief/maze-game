63a83c3d7f8f2819fa1ba6e4e1510f0bbb366fb6
Philipp Kief <philipp.kief@gmx.de>
2022-10-03 09:40:06 +0200

    Implement canvas element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'maze-game';
+export class AppComponent implements OnInit {
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement> | undefined;
+
+  private ctx: CanvasRenderingContext2D | undefined | null;
+
+  ngOnInit() {
+    if (this.canvas) {
+      this.canvas.nativeElement.width = 500;
+      this.canvas.nativeElement.height = 500;
+    }
+    this.ctx = this.canvas?.nativeElement?.getContext('2d');
+    if (this.ctx) {
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillRect(0, 0, 500, 500);
+    }
+  }
 }
